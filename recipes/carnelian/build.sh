e28a8eee5d51b85969b6008418d4f3a778648f74
Sumaiya Nazeen <nazeen.777@gmail.com>
2018-08-16 13:59:48 -0400

    update env

--- recipes/carnelian/build.sh
+++ recipes/carnelian/build.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-#git clone https://github.com/JohnLangford/vowpal_wabbit.git
-#cd vowpal_wabbit
-#./autogen.sh
-#./configure
-#make
-#make install
-#cd ..
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+export LD_LIBRARY_PATH=${PREFIX}/local/lib
+
+git clone https://github.com/JohnLangford/vowpal_wabbit.git
+cd vowpal_wabbit
+./autogen.sh
+./configure
+make
+make install
+cd ..
+
 conda install -c bioconda emboss
+
 echo ========================================
 echo Setting up Genetic Data analysis Library and compiling utility tools
 echo ========================================
+
 cd util
 sh INSTALL.sh
 cd ..
+
 echo ========================================
 echo Downloading and extracting gold standard datasets
 echo ========================================
+
 mkdir data
 cd data
 wget http://giant.csail.mit.edu/carnelian/EC-2192-DB.tar.gz
