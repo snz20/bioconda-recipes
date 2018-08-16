eac8561cb38dfdf8462fa4d92d0524623a3366ed
Sumaiya Nazeen <nazeen.777@gmail.com>
2018-08-16 12:40:45 -0400

    add carnelian

---
+++ recipes/carnelian/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+echo ========================================
+echo Setting up Genetic Data analysis Library and compiling utility tools
+echo ========================================
+cd util
+sh INSTALL.sh
+cd ..
+echo ========================================
+echo Downloading and extracting gold standard datasets
+echo ========================================
+mkdir data
+cd data
+wget http://giant.csail.mit.edu/carnelian/EC-2192-DB.tar.gz
+tar -zxvf EC-2192-DB.tar.gz
+rm EC-2192-DB.tar.gz
+cd ..
